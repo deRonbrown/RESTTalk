0098a48d547cc8b887ede8ad9520170f5e81489c
DeRon Brown <deron.m.brown@espn.com>
2012-05-16 21:31:37 -0400

    Create rest response class

--- helpers/response.rb
+++ helpers/response.rb
@@ -1,19 +1,12 @@
 class TalkResponse < Object
 
-  attr_accessor :url , :status, :status_code, :result, :callback, :numResults, :resultsPerQuery
-  def initialize url, callback = nil, status=:ok, result={}, numResults=0, resultsPerQuery=0
+  attr_accessor :url , :status, :status_code, :result
+  def initialize url, status=:ok, result={}
     @url = url
     @status = status
-    @callback = callback
     @status_code = 200
-    @numResults = numResults
-    @resultsPerQuery = resultsPerQuery
     @result = result
     @errors = []
-  end
-
-  def is_jsonp?
-    not @callback.nil?
   end
 
   def add_error hash
@@ -22,16 +15,12 @@ class TalkResponse < Object
   end
 
   def to_response
-    if @callback
-      "#{@callback}(#{@result.to_json})"
-    else
-      to_json
-    end
+    to_json
   end
   
 
   def to_json(*a)
-    hash = {"url" => @url, "status"=>@status.to_s, "resultsPerQuery"=>@resultsPerQuery, "numResults"=>@numResults} 
+    hash = {"url" => @url, "status"=>@status.to_s} 
     if @status == :ok
       hash["result"] = @result
     else
